2d873037d49e52c4b4878a3311b152e354ba38c3
rafalszmit <rafal.szmit@testarmy.com>
2018-07-09 15:13:48 +0200

    fixed tests

--- spec/AppSpec.js
+++ spec/AppSpec.js
@@ -3,12 +3,12 @@ describe("Pesel generator", function () {
 
     it("should return female pesel", function () {
         pesel = generatePesel(new Date("1963-09-07"), "F",111);
-        expect(pesel[10] % 2).toBe(0);
+        expect(pesel[9] % 2).toBe(0);
     });
 
     it("should return male pesel", function () {
         pesel = generatePesel(new Date("1963-09-07"), "M",111);
-        expect(pesel[10] % 2).not.toBe(0);
+        expect(pesel[9] % 2).not.toBe(0);
     });
 
     it("should return pesel with valid control sum", function () {
@@ -23,23 +23,23 @@ describe("Pesel generator", function () {
     });
 
     it("should return valid pesel for someone born between 1800 and 1899", function () {
-        pesel = generatePesel(new Date("1899-01-02"), "M",111);
-        expect(pesel[2]).not.toBe(8);
+        pesel = generatePesel(new Date("1899-12-02"), "M",111);
+        expect(pesel[2]).toBe('9');
     });
 
     it("should return valid pesel for someone born between 2000 and 2099", function () {
         pesel = generatePesel(new Date("2000-01-02"), "M",111);
-        expect(pesel[2]).not.toBe(2);
+        expect(pesel[2]).toBe('2');
     });
 
     it("should return valid pesel for someone born between 2100 and 2199", function () {
         pesel = generatePesel(new Date("2150-01-02"), "M",111);
-        expect(pesel[2]).not.toBe(4);
+        expect(pesel[2]).toBe('4');
     });
 
     it("should return valid pesel for someone born between 2200 and 2299", function () {
         pesel = generatePesel(new Date("2299-01-02"), "M",111);
-        expect(pesel[2]).not.toBe(6);
+        expect(pesel[2]).toBe('6');
     });
 
     
