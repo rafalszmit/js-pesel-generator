23d59a90990f37e0e028a74aa7b085545ce31581
rafalszmit <rafal.szmit@testarmy.com>
2018-07-13 10:27:58 +0200

    applied sugestions

--- spec/AppSpec.js
+++ spec/AppSpec.js
@@ -6,11 +6,9 @@
 describe("Pesel generator", function () {
 
     var generatePesel = require('../app.js');
-    const genderDigitPosition = 9;
-    const controlSumPosition = 10;
 
     function isEven(num) {
-        return num % 2;
+        return num % 2 == 0;
     }
 
     function getMonth(pesel) {
@@ -21,38 +19,42 @@ describe("Pesel generator", function () {
         return pesel.substr(-1);
     }
 
+    function getGenderDigit(pesel) {
+        return pesel.substr(-3);
+    }
+
     it("should return female pesel", function () {
         let pesel = generatePesel(new Date("1963-09-07"), "F", 111);
-        expect(isEven(pesel[genderDigitPosition])).toBe(0);
+        expect(isEven(getGenderDigit(pesel))).toBeTruthy();
     });
 
     it("should return male pesel", function () {
         let pesel = generatePesel(new Date("1963-09-07"), "M", 111);
-        expect(isEven(pesel[genderDigitPosition])).not.toBe(0);
+        expect(isEven(getGenderDigit(pesel))).toBeFalsy();
     });
 
     it("should return pesel with valid control sum", function () {
         let pesel = generatePesel(new Date("1980-01-12"), 1, 743);
         let expectedPesel = '80011274315';//generated with http://pesel.felis-net.com/
-        expect(pesel[controlSumPosition]).toEqual((getControlSum(expectedPesel)).toString());
+        expect(getControlSum(pesel)).toEqual(getControlSum(expectedPesel));
     });
 
-    it("should return valid pesel for someone born between 1800 and 1899", function () {
+    it("should return pesel with month number increased by 80 for someone born between 1800 and 1899", function () {
         let pesel = generatePesel(new Date("1899-12-02"), "M", 111);
         expect(getMonth(pesel)).toBe('92');
     });
 
-    it("should return valid pesel for someone born between 2000 and 2099", function () {
+    it("should return pesel with month number increased by 20 for someone born between 2000 and 2099", function () {
         let pesel = generatePesel(new Date("2000-01-02"), "M", 111);
         expect(getMonth(pesel)).toBe('21');
     });
 
-    it("should return valid pesel for someone born between 2100 and 2199", function () {
+    it("should return pesel with month number increased by 40 for someone born between 2100 and 2199", function () {
         let pesel = generatePesel(new Date("2150-01-02"), "M", 111);
         expect(getMonth(pesel)).toBe('41');
     });
 
-    it("should return valid pesel for someone born between 2200 and 2299", function () {
+    it("should return pesel with month number increased by 60 for someone born between 2200 and 2299", function () {
         let pesel = generatePesel(new Date("2299-01-02"), "M", 111);
         expect(getMonth(pesel)).toBe('61');
     });
