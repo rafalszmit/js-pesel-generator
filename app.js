07e03dc03c3f236a16d4469baada0edee8486d64
rafalszmit <rafal.szmit@testarmy.com>
2018-07-19 15:10:44 +0200

    changed getControlSum function to use reduce()

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 /*jslint es6 */
 "use strict";
-function generatePesel(date, sex, nr) {
-    var year = fillZeros(getValidYearInTwoDigits(date));
-    var month = fillZeros(getValidMonth(date));
-    var day = fillZeros(date.getDate());
-    var genderDigit = getDigitForGender(sex);
-    var pesel = `${year}${month}${day}${nr}${genderDigit}`;
-    pesel += getControlSum(pesel);
-    return pesel;
-}
-
 function fillZeros(value) {
     if (value < 10) {
         value = "0" + value;
@@ -41,28 +31,32 @@ function getValidYearInTwoDigits(date) {
 
 function getControlSum(notFullPesel) {
     var wagi = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];
-    var controlSum = 0;
-    var tmp = 0;
-    for (var i = 0; i < notFullPesel.length; i++) {
-        tmp += notFullPesel[i] * wagi[i];
-    }
-    controlSum = tmp % 10;
-    return controlSum;
+    return wagi.map((a, b) => a * notFullPesel[b]).reduce((sum, i) => sum + i) % 10;
 }
 
 function getDigitForGender(genderDigit) {
-    var female = [0, 2, 4, 6, 8]
-    var male = [1, 3, 5, 7, 9]
+    var female = [0, 2, 4, 6, 8];
+    var male = [1, 3, 5, 7, 9];
 
-    if (genderDigit == "F") {
-        genderDigit = female[Math.floor(Math.random() * female.length)]
+    if (genderDigit === "F") {
+        genderDigit = female[Math.floor(Math.random() * female.length)];
     }
-    if (genderDigit == "M") {
-        genderDigit = male[Math.floor(Math.random() * male.length)]
+    if (genderDigit === "M") {
+        genderDigit = male[Math.floor(Math.random() * male.length)];
     }
     return genderDigit;
 }
 
+function generatePesel(date, sex, nr) {
+    var year = fillZeros(getValidYearInTwoDigits(date));
+    var month = fillZeros(getValidMonth(date));
+    var day = fillZeros(date.getDate());
+    var genderDigit = getDigitForGender(sex);
+    var pesel = `${year}${month}${day}${nr}${genderDigit}`;
+    pesel += getControlSum(pesel);
+    return pesel;
+}
+
 module.exports = generatePesel;
-//console.log("F: " + generatePesel(new Date("2101-03-25"), "F", 111));
-//console.log("M: " + generatePesel(new Date("1963-09-07"), "M", 999));
+// console.log("F: " + generatePesel(new Date("2101-03-25"), "F", 111));
+// console.log("M: " + generatePesel(new Date("1963-09-07"), "M", 999));
